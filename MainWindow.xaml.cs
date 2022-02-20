74f062c10235e94a149a7539b2d6bbd368cd5dab
RemoteConnection <letuanmanh86@gmail.com>
2022-02-20 11:17:29 +0700

    modify logic man-auto: delete button man

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -54,6 +54,8 @@ namespace Scada_TM221_WaterFllow
         /// </summary>
         private void BeginEnableButton()
         {
+            BtAutoOn.IsEnabled = false;
+            BtAutoOff.IsEnabled = false;
             Btplc40off.IsEnabled = false;
             Btplc24off.IsEnabled = false;
             BtEventer1off.IsEnabled = false;
@@ -72,7 +74,9 @@ namespace Scada_TM221_WaterFllow
             BtPumb5on.IsEnabled = false;
             BtPumb6off.IsEnabled = false;
             BtPumb6on.IsEnabled = false;
-
+            AnimationHightWaterTank.Color = Colors.LightGray;
+            AnimationLowtWaterTank.Color = Colors.LightGray;
+            AnimationErrorPhaseStation2.Color = Colors.LightGray;
             Btnt1off.IsEnabled = false;
             Btnt1on.IsEnabled = false;
             Btnt2off.IsEnabled = false;
@@ -80,7 +84,6 @@ namespace Scada_TM221_WaterFllow
             HightWaterRawAnimation.Color = Colors.LightGray;
             AnimationLowWater.Color = Colors.LightGray;
             ErrorPhaseAnimation.Color = Colors.LightGray;
-
         }
 
         /// <summary>
@@ -88,14 +91,14 @@ namespace Scada_TM221_WaterFllow
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void ManTM40(Object sender, RoutedEventArgs e)
+        private void AutoTM40Off(Object sender, RoutedEventArgs e)
         {
             if(modbusClientTM40.Connected)
             {
                 modbusClientTM40.WriteSingleCoil(53, true);
                 modbusClientTM40.WriteSingleCoil(54, false);
-                BtMan.IsEnabled = false;
-                BtAuto.IsEnabled = true;
+                BtAutoOn.IsEnabled = true;
+                BtAutoOff.IsEnabled = false;
                 BtEventer1off.IsEnabled = false;
                 BtEventer1on.IsEnabled = true;
                 BtPumb1off.IsEnabled = false;
@@ -114,14 +117,14 @@ namespace Scada_TM221_WaterFllow
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void AutoTM40(object sender, RoutedEventArgs e)
+        private void AutoTM40On(object sender, RoutedEventArgs e)
         {
             if(modbusClientTM40.Connected)
             {
                 modbusClientTM40.WriteSingleCoil(53, false);
                 modbusClientTM40.WriteSingleCoil(54, true);
-                BtAuto.IsEnabled = false;
-                BtMan.IsEnabled = true;
+                BtAutoOn.IsEnabled = false;
+                BtAutoOff.IsEnabled = true;
                 BtEventer1off.IsEnabled = false;
                 BtEventer1on.IsEnabled = false;
                 BtPumb1on.IsEnabled = false;
@@ -145,9 +148,10 @@ namespace Scada_TM221_WaterFllow
             try
             {
                 modbusClientTM40.Connect();
-                CompositionTarget.Rendering += OnTimedEventTM40;
                 Btplc40off.IsEnabled = true;
                 Btplc40on.IsEnabled = false;
+                BtAutoOn.IsEnabled = true;
+                CompositionTarget.Rendering += OnTimedEventTM40;
             } catch {MessageBox.Show("Can't connect PLC TM221CE40R, You. Please check your hardware connection!!!");}
         }
         /// <summary>
@@ -157,11 +161,10 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void DisconnectTM40(object sender, RoutedEventArgs e)
         {
-
-            modbusClientTM24.Disconnect();
+            BtAutoOn.IsEnabled = false;
+            BtAutoOff.IsEnabled = false;
             Btplc40on.IsEnabled = true;
             Btplc40off.IsEnabled = false;
-
             BtEventer1off.IsEnabled = false;
             BtEventer1on.IsEnabled = false;
             BtPumb1off.IsEnabled = false;
@@ -178,6 +181,7 @@ namespace Scada_TM221_WaterFllow
             BtPumb5on.IsEnabled = false;
             BtPumb6off.IsEnabled = false;
             BtPumb6on.IsEnabled = false;
+            modbusClientTM40.Disconnect();
         }
         /// <summary>
         /// Connected PLC TM221CE24R
@@ -192,28 +196,167 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void OnTimedEventTM40(object sender, EventArgs e)
         {
-            if (modbusClientTM40.Connected)
+            try
             {
-                singleCoilTM40.M0 = modbusClientTM40.ReadCoils(0, 1)[0];
-                singleCoilTM40.M1 = modbusClientTM40.ReadCoils(1, 1)[0];
-                singleCoilTM40.M2 = modbusClientTM40.ReadCoils(2, 1)[0];
-                singleCoilTM40.M3 = modbusClientTM40.ReadCoils(3, 1)[0];
-                singleCoilTM40.M4 = modbusClientTM40.ReadCoils(4, 1)[0];
-                singleCoilTM40.M5 = modbusClientTM40.ReadCoils(5, 1)[0];
-                singleCoilTM40.M6 = modbusClientTM40.ReadCoils(6, 1)[0];
-                singleCoilTM40.M7 = modbusClientTM40.ReadCoils(7, 1)[0];
-                singleCoilTM40.M8 = modbusClientTM40.ReadCoils(8, 1)[0];
-                singleCoilTM40.M9 = modbusClientTM40.ReadCoils(9, 1)[0];
-                singleCoilTM40.M10 = modbusClientTM40.ReadCoils(10, 1)[0];
-                singleCoilTM40.M11 = modbusClientTM40.ReadCoils(11, 1)[0];
-                singleCoilTM40.M12 = modbusClientTM40.ReadCoils(12, 1)[0];
-                singleCoilTM40.M14 = modbusClientTM40.ReadCoils(14, 1)[0];
-                singleCoilTM40.M15 = modbusClientTM40.ReadCoils(15, 1)[0];
-                singleCoilTM40.M16 = modbusClientTM40.ReadCoils(16, 1)[0];
-                singleCoilTM40.M17 = modbusClientTM40.ReadCoils(17, 1)[0];
-                singleCoilTM40.M18 = modbusClientTM40.ReadCoils(18, 1)[0];
-                singleCoilTM40.M19 = modbusClientTM40.ReadCoils(19, 1)[0];
+                if (modbusClientTM40.Connected == true)
+                {
+                    singleCoilTM40.M0 = modbusClientTM40.ReadCoils(0, 1)[0];
+                    if (singleCoilTM40.M0 == true)
+                    {
+                        AnimationErrorPhaseStation2.Color = Color.FromRgb((byte)_rand.Next(255), (byte)_rand.Next(255), (byte)_rand.Next(255));
+                    }
+                    else if (singleCoilTM40.M1 == false)
+                    {
+                        AnimationErrorPhaseStation2.Color = Colors.MediumSpringGreen;
+                    }
+                    singleCoilTM40.M1 = modbusClientTM40.ReadCoils(1, 1)[0];
+                    if (singleCoilTM40.M1 == true)
+                    {
+                        AnimationHightWaterTank.Color = Colors.LightGray;
+                        AnimationLowtWaterTank.Color = Color.FromRgb((byte)_rand.Next(255), (byte)_rand.Next(255), (byte)_rand.Next(255));
+                    }
+                    else if (singleCoilTM40.M1 == false)
+                    {
+                        AnimationHightWaterTank.Color = Colors.MediumSpringGreen;
+                        AnimationLowtWaterTank.Color = Colors.LightGray;
+                    }
+                    singleCoilTM40.M2 = modbusClientTM40.ReadCoils(2, 1)[0];
+                    singleCoilTM40.M3 = modbusClientTM40.ReadCoils(3, 1)[0];
+                    singleCoilTM40.M4 = modbusClientTM40.ReadCoils(4, 1)[0];
+                    singleCoilTM40.M5 = modbusClientTM40.ReadCoils(5, 1)[0];
+                    if (singleCoilTM40.M5 == true)
+                    {
+                        BtEventer1on.IsEnabled = false;
+
+                        BtEventer1off.IsEnabled = true;
+                    }
+                    else if (singleCoilTM40.M5 == false)
+                    {
+                        BtEventer1on.IsEnabled = true;
+                        BtEventer1off.IsEnabled = false;
+                    }
+                    singleCoilTM40.M6 = modbusClientTM40.ReadCoils(6, 1)[0];
+                    if (singleCoilTM40.M6 == true)
+                    {
+                        BtEventer2on.IsEnabled = false;
+
+                        BtEventer2off.IsEnabled = true;
+                    }
+                    else if (singleCoilTM40.M6 == false)
+                    {
+                        BtEventer2on.IsEnabled = true;
+                        BtEventer2off.IsEnabled = false;
+                    }
+                    singleCoilTM40.M7 = modbusClientTM40.ReadCoils(7, 1)[0];
+                    if (singleCoilTM40.M7 == true)
+                    {
+                        BtPumb1on.IsEnabled = false;
+
+                        BtPumb1off.IsEnabled = true;
+                    }
+                    else if (singleCoilTM40.M7 == false)
+                    {
+                        BtPumb1on.IsEnabled = true;
+                        BtPumb1off.IsEnabled = false;
+                    }
+                    singleCoilTM40.M8 = modbusClientTM40.ReadCoils(8, 1)[0];
+                    if (singleCoilTM40.M8 == true)
+                    {
+                        BtPumb2on.IsEnabled = false;
+
+                        BtPumb2off.IsEnabled = true;
+                    }
+                    else if (singleCoilTM40.M8 == false)
+                    {
+                        BtPumb2on.IsEnabled = true;
+                        BtPumb2off.IsEnabled = false;
+                    }
+                    singleCoilTM40.M9 = modbusClientTM40.ReadCoils(9, 1)[0];
+                    if (singleCoilTM40.M9 == true)
+                    {
+                        BtPumb3on.IsEnabled = false;
+
+                        BtPumb3off.IsEnabled = true;
+                    }
+                    else if (singleCoilTM40.M9 == false)
+                    {
+                        BtPumb3on.IsEnabled = true;
+                        BtPumb3off.IsEnabled = false;
+                    }
+                    singleCoilTM40.M10 = modbusClientTM40.ReadCoils(10, 1)[0];
+                    if (singleCoilTM40.M10 == true)
+                    {
+                        BtPumb4on.IsEnabled = false;
+
+                        BtPumb4off.IsEnabled = true;
+                    }
+                    else if (singleCoilTM40.M10 == false)
+                    {
+                        BtPumb4on.IsEnabled = true;
+                        BtPumb4off.IsEnabled = false;
+                    }
+                    singleCoilTM40.M11 = modbusClientTM40.ReadCoils(11, 1)[0];
+                    if (singleCoilTM40.M11 == true)
+                    {
+                        BtPumb5on.IsEnabled = false;
+
+                        BtPumb5off.IsEnabled = true;
+                    }
+                    else if (singleCoilTM40.M11 == false)
+                    {
+                        BtPumb5on.IsEnabled = true;
+                        BtPumb5off.IsEnabled = false;
+                    }
+                    singleCoilTM40.M12 = modbusClientTM40.ReadCoils(12, 1)[0];
+                    if (singleCoilTM40.M12 == true)
+                    {
+                        BtPumb6on.IsEnabled = false;
+
+                        BtPumb6off.IsEnabled = true;
+                    }
+                    else if (singleCoilTM40.M12 == false)
+                    {
+                        BtPumb6on.IsEnabled = true;
+                        BtPumb6off.IsEnabled = false;
+                    }
+                    singleCoilTM40.M14 = modbusClientTM40.ReadCoils(14, 1)[0];
+                    singleCoilTM40.M15 = modbusClientTM40.ReadCoils(15, 1)[0];
+                    singleCoilTM40.M16 = modbusClientTM40.ReadCoils(16, 1)[0];
+                    singleCoilTM40.M17 = modbusClientTM40.ReadCoils(17, 1)[0];
+                    singleCoilTM40.M18 = modbusClientTM40.ReadCoils(18, 1)[0];
+                    singleCoilTM40.M19 = modbusClientTM40.ReadCoils(19, 1)[0];
+                    singleCoilTM40.M53 = modbusClientTM40.ReadCoils(53, 1)[0];
+                    singleCoilTM40.M54 = modbusClientTM40.ReadCoils(54, 1)[0];
+                } else if (modbusClientTM40.Connected == false)
+                {
+                    Btplc40off.IsEnabled = false;
+                    Btplc24off.IsEnabled = false;
+                    BtEventer1off.IsEnabled = false;
+                    BtEventer1on.IsEnabled = false;
+                    BtPumb1off.IsEnabled = false;
+                    BtPumb1on.IsEnabled = false;
+                    BtEventer2off.IsEnabled = false;
+                    BtEventer2on.IsEnabled = false;
+                    BtPumb2off.IsEnabled = false;
+                    BtPumb2on.IsEnabled = false;
+                    BtPumb3off.IsEnabled = false;
+                    BtPumb3on.IsEnabled = false;
+                    BtPumb4off.IsEnabled = false;
+                    BtPumb4on.IsEnabled = false;
+                    BtPumb5off.IsEnabled = false;
+                    BtPumb5on.IsEnabled = false;
+                    BtPumb6off.IsEnabled = false;
+                    BtPumb6on.IsEnabled = false;
+                    AnimationHightWaterTank.Color = Colors.LightGray;
+                    AnimationLowtWaterTank.Color= Colors.LightGray;
+                    AnimationErrorPhaseStation2.Color = Colors.LightGray;
+                }
+            } catch
+            {
+                modbusClientTM40.Disconnect();
+                MessageBox.Show("Can't connect PLC TM221CE24R, You. Please check your hardware connection!!!");
             }
+
         }
 
         /// <summary>
@@ -223,7 +366,7 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void Eventer_1_On(Object sender, RoutedEventArgs e)
         {
-            if(modbusClientTM40.Connected)
+            if(modbusClientTM40.Connected || singleCoilTM40.M54 == true || singleCoilTM40.M53 == false)
             {
                 modbusClientTM40.WriteSingleCoil(22, true);
                 BtEventer1on.IsEnabled = false;
@@ -237,7 +380,7 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void Eventer_1_Off(Object sender, RoutedEventArgs e)
         {
-            if (modbusClientTM40.Connected)
+            if (modbusClientTM40.Connected || singleCoilTM40.M54 == true || singleCoilTM40.M53 == false)
             {
                 modbusClientTM40.WriteSingleCoil(22, false);
                 BtEventer1off.IsEnabled = false;    
@@ -251,7 +394,7 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void Pumb_1_On(Object sender, RoutedEventArgs e)
         {
-            if (modbusClientTM40.Connected)
+            if (modbusClientTM40.Connected || singleCoilTM40.M54 == true || singleCoilTM40.M53 == false)
             {
                 modbusClientTM40.WriteSingleCoil(21, true);
                 BtPumb1on.IsEnabled = false;
@@ -265,7 +408,7 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void Pumb_1_Off(Object sender, RoutedEventArgs e)
         {
-            if (modbusClientTM40.Connected)
+            if (modbusClientTM40.Connected || singleCoilTM40.M54 == true || singleCoilTM40.M53 == false)
             {
                 modbusClientTM40.WriteSingleCoil(21, false);
                 BtPumb1on.IsEnabled = true;
@@ -279,7 +422,7 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void Eventer_2_On(Object sender, RoutedEventArgs e)
         {
-            if (modbusClientTM40.Connected)
+            if (modbusClientTM40.Connected || singleCoilTM40.M54 == true || singleCoilTM40.M53 == false)
             {
                 modbusClientTM40.WriteSingleCoil(24, true);
                 BtEventer2on.IsEnabled = false;
@@ -294,7 +437,7 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void Eventer_2_Off(Object sender, RoutedEventArgs e)
         {
-            if (modbusClientTM40.Connected)
+            if (modbusClientTM40.Connected || singleCoilTM40.M54 == true || singleCoilTM40.M53 == false)
             {
                 modbusClientTM40.WriteSingleCoil(24, false);
                 BtEventer2off.IsEnabled = false;
@@ -308,7 +451,7 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void Pumb_2_On(Object sender, RoutedEventArgs e)
         {
-            if (modbusClientTM40.Connected)
+            if (modbusClientTM40.Connected || singleCoilTM40.M54 == true || singleCoilTM40.M53 == false)
             {
                 modbusClientTM40.WriteSingleCoil(23, true);
                 BtPumb2on.IsEnabled = false;
@@ -322,7 +465,7 @@ namespace Scada_TM221_WaterFllow
         /// <param name="e"></param>
         private void Pumb_2_Off(Object sender, RoutedEventArgs e)
         {
-            if (modbusClientTM40.Connected)
+            if (modbusClientTM40.Connected || singleCoilTM40.M54 == true || singleCoilTM40.M53 == false)
             {
                 modbusClientTM40.WriteSingleCoil(23, false);
                 BtPumb2on.IsEnabled = true;
@@ -448,8 +591,6 @@ namespace Scada_TM221_WaterFllow
 
 
 
-        //=============================================================================================================================================
-
         private void ConnectTM24(object sender, RoutedEventArgs e)
         {
             try
@@ -476,7 +617,6 @@ namespace Scada_TM221_WaterFllow
             Btnt2off.IsEnabled = false;
             modbusClientTM24.Disconnect();
         }
-
         /// <summary>
         /// Timer request read single coil TM221CE24R
         /// </summary>
@@ -553,7 +693,6 @@ namespace Scada_TM221_WaterFllow
                 MessageBox.Show("Can't connect PLC TM221CE24R, You. Please check your hardware connection!!!"); 
             }
         }
-
         /// <summary>
         /// Start nuoc tho 1
         /// </summary>
@@ -595,7 +734,6 @@ namespace Scada_TM221_WaterFllow
                 Btnt2off.IsEnabled = true;
                 Btnt2on.IsEnabled = false;
             } else { MessageBox.Show("You are not connected to PLC TM221CE24R"); }
-
         }
         /// <summary>
         /// Stop nuoc tho 2
