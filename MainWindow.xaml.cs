9604468b69afcfbf4f9b30b8ff2a7dfbbe54534a
letuan duong <dientutnd@gmail.com>
2022-04-07 23:59:39 +0700

    add trigger data M5 pumb nt 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using EasyModbus;
-using System.Timers;
-using System.Windows.Media.Animation;
 
 namespace Scada_TM221_WaterFllow
 {
@@ -58,6 +46,8 @@ namespace Scada_TM221_WaterFllow
             Persure.DataContext = singleCoilTM40;
             canvasdata.DataContext = singleCoilTM24;
             Group_Wiring_Box.DataContext = singleCoilTM40;
+
+
             this.DataContext = this;
 
             List<Pumb> pumbs = new List<Pumb>();
